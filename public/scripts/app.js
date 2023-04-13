2dd0d4e3095c455ef993d135226e150a600294ce
andy-wanganqi <andy.wanganqi@gmail.com>
2023-04-13 21:40:57 +1200

    11. JSX Expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,6 +9,20 @@ var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.
 //   p -> age
 //   p -> location
 
-var template2 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Andy Wang"), /*#__PURE__*/React.createElement("p", null, "Age: 40"), /*#__PURE__*/React.createElement("p", null, "Location: Auckland"));
+var user = {
+  firstName: 'Andy',
+  lastName: 'Wang',
+  age: 40,
+  location: 'Auckland'
+};
+var template2 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.firstName.toUpperCase(), ", ", user.lastName), /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), /*#__PURE__*/React.createElement("p", null, "Location: ", user.location));
+
+// Create another object: title/subtitle
+// Use title/subtitle into the template
+var obj3 = {
+  title: 'Indecision APp',
+  subtitle: 'Put your life in the hands of a computer'
+};
+var template3 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, obj3.title.toUpperCase()), /*#__PURE__*/React.createElement("p", null, obj3.subtitle.toLowerCase()));
 var appRoot = document.getElementById('app');
-ReactDOM.render(template2, appRoot);
+ReactDOM.render(template3, appRoot);
--- src/app.jsx
+++ src/app.jsx
@@ -16,13 +16,32 @@ let template =
 //   p -> age
 //   p -> location
 
+let user = {
+  firstName: 'Andy',
+  lastName: 'Wang',
+  age: 40,
+  location: 'Auckland'
+};
+
 let template2 = 
 <div>
-  <h1>Andy Wang</h1>
-  <p>Age: 40</p>
-  <p>Location: Auckland</p>
+  <h1>{user.firstName.toUpperCase()}, {user.lastName}</h1>
+  <p>Age: {user.age}</p>
+  <p>Location: {user.location}</p>
+</div>;
+
+// Create another object: title/subtitle
+// Use title/subtitle into the template
+let obj3 = {
+  title: 'Indecision APp',
+  subtitle: 'Put your life in the hands of a computer'
+}
+let template3 = 
+<div>
+  <p>{obj3.title.toUpperCase()}</p>
+  <p>{obj3.subtitle.toLowerCase()}</p>
 </div>;
 
 
 let appRoot = document.getElementById('app');
-ReactDOM.render(template2, appRoot);
+ReactDOM.render(template3, appRoot);
