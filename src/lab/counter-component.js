1194e34411f3268afd3d16f6c87f38fc32dec2c4
andy-wanganqi <andy.wanganqi@gmail.com>
2023-04-21 21:41:01 +1200

    46. Saving and Loading the count

--- src/lab/counter-component.js
+++ src/lab/counter-component.js
@@ -5,10 +5,22 @@ class Counter extends React.Component {
     this.handleReset = this.handleReset.bind(this)
     this.handleMinusOne = this.handleMinusOne.bind(this)
     this.state = {
-      count: props.count || 0,
-      name: 'Andy'
+      count: 0
     }
   }
+  componentDidMount() {
+    const count = parseInt(localStorage.getItem('count'))
+    if (!isNaN(count)) {
+      this.setState(() => ({
+        count
+      }))
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count)
+    }
+  }  
   handlePlusOne(){
     console.log('> handlePlusOne')
     // Updating but NOT replacing the state
@@ -48,7 +60,4 @@ class Counter extends React.Component {
   }
 }
 
-Counter.defaultProps = {
-  count: 100
-}
-ReactDOM.render(<Counter count={1} />, document.getElementById('app'))
+ReactDOM.render(<Counter />, document.getElementById('app'))
