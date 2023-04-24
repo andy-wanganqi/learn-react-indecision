804b542e69de50d582644fd40476d5f094e9bb02
andy-wanganqi <andy.wanganqi@gmail.com>
2023-04-24 22:13:51 +1200

    ﻿53. Importing npm Modules

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import minusRenamedFromDefault, { square, add } from './utils.js'
 import isSenior, { isAdult, canDrive } from './person.js'
+import validator from 'validator'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 console.log('hello webpack')
 
-console.log(square(4))
-console.log(add(4, 11))
-console.log(minusRenamedFromDefault(11, 4))
+console.log('square: ', square(4))
+console.log('add: ', add(4, 11))
+console.log('minusRenamedFromDefault: ', minusRenamedFromDefault(11, 4))
 
 console.log(isAdult(4))
 console.log(canDrive(4))
 console.log(isSenior(70))
+
+console.log('isAdult: ', isAdult(4))
+console.log('canDrive: ', canDrive(4))
+console.log('isSenior: ', isSenior(70))
+
+console.log('isEmail: ', validator.isEmail('aaa@gmail.com'))
 
 // git rebase test
