07cdec2878dff90e9ce6e2870af056053be31e9f
andy-wanganqi <andy.wanganqi@gmail.com>
2023-04-24 16:33:02 +1200

    51. ES6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+import { square, add } from './utils.js'
+import { isAdult, canDrive } from './person.js'
+
 console.log('hello webpack')
+
+console.log(square(4))
+console.log(add(4, 11))
+
+console.log(isAdult(4))
+console.log(canDrive(4))
---
+++ src/person.js
@@ -0,0 +1,5 @@
+console.log('person.js is running')
+
+export const isAdult = (age) => age >= 18
+
+export const canDrive = (age) => age >= 20
